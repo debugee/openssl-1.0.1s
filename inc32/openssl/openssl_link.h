179ea1f0a25a079c3d26be542909425b7f09591c
debugee <519306795@qq.com>
2023-07-23 20:13:12 +0800

    lib

--- inc32/openssl/openssl_link.h
+++ inc32/openssl/openssl_link.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #ifdef _DEBUG
-	#pragma comment(lib, "libeay32_mtd.lib")
-	#pragma comment(lib, "ssleay32_mtd.lib")
+	#pragma comment(lib, "v12_smdd_libeay32.lib")
+	#pragma comment(lib, "v12_smdd_ssleay32.lib")
 #else
-	#pragma comment(lib, "libeay32_mt.lib")
-	#pragma comment(lib, "ssleay32_mt.lib")
+	#pragma comment(lib, "v12_smd_libeay32.lib")
+	#pragma comment(lib, "v12_smd_ssleay32.lib")
 #endif
 
