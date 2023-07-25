7ad25d61ae5cdc6523e0bdfa0844a7c5cd48523a
debugee <519306795@qq.com>
2023-07-25 20:01:23 +0800

    link.h

--- inc32/openssl/openssl_link.h
+++ inc32/openssl/openssl_link.h
@@ -1,10 +1,39 @@
 #pragma once
 
 #ifdef _DEBUG
-	#pragma comment(lib, "v12_smdd_libeay32.lib")
-	#pragma comment(lib, "v12_smdd_ssleay32.lib")
+	#ifdef _DLL
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smdd_libeay32.lib")
+			#pragma comment(lib, "v12_smdd_ssleay32.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smdd_libeay32.lib")
+			#pragma comment(lib, "v14_smdd_ssleay32.lib")
+		#endif
+	#else
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smtd_libeay32.lib")
+			#pragma comment(lib, "v12_smtd_ssleay32.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smtd_libeay32.lib")
+			#pragma comment(lib, "v14_smtd_ssleay32.lib")
+		#endif
+	#endif
 #else
-	#pragma comment(lib, "v12_smd_libeay32.lib")
-	#pragma comment(lib, "v12_smd_ssleay32.lib")
+	#ifdef _DLL
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smd_libeay32.lib")
+			#pragma comment(lib, "v12_smd_ssleay32.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smd_libeay32.lib")
+			#pragma comment(lib, "v14_smd_ssleay32.lib")
+		#endif
+	#else
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smt_libeay32.lib")
+			#pragma comment(lib, "v12_smt_ssleay32.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smt_libeay32.lib")
+			#pragma comment(lib, "v14_smt_ssleay32.lib")
+		#endif
+	#endif
 #endif
-
